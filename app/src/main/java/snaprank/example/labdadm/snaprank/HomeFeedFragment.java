7b6a5313fecccad34f8df7f467ad2c4c57997a17
coque <coquejardon@gmail.com>
2019-03-19 18:37:54 +0100

    Ventana "ViewPic" acabada

--- app/src/main/java/snaprank/example/labdadm/snaprank/HomeFeedFragment.java
+++ app/src/main/java/snaprank/example/labdadm/snaprank/HomeFeedFragment.java
@@ -1,17 +1,36 @@
 package snaprank.example.labdadm.snaprank;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class HomeFeedFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,null);
+        View view = inflater.inflate(R.layout.fragment_home,container, false);
+        ImageView image = view.findViewById(R.id.main_pic);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoViewPic(v);
+            }
+        });
+
+        return view;
+
+    }
+
+    public void gotoViewPic(View view){
+        Intent intent = new Intent(getContext(), ViewPic.class);
+        startActivity(intent);
     }
 }
