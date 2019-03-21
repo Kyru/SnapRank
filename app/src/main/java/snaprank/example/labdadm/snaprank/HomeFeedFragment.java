19c930ee54597d1944ec6a4f273f5508c7bd54c1
Fer <ferraniusd@gmail.com>
2019-03-21 22:08:07 +0100

    Primera versión del popup filter implementado
    Además:
    + Borrado el oldCustomNavigation porque no se estaba utilizando
    + Modificado el TODO

--- app/src/main/java/snaprank/example/labdadm/snaprank/HomeFeedFragment.java
+++ app/src/main/java/snaprank/example/labdadm/snaprank/HomeFeedFragment.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class HomeFeedFragment extends Fragment {
+
+    ImageButton ib_filter;
 
     @Nullable
     @Override
@@ -25,6 +31,15 @@ public class HomeFeedFragment extends Fragment {
             }
         });
 
+
+        ib_filter = ((AppCompatActivity)getActivity()).findViewById(R.id.custom_bar_filter);
+        ib_filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopUpMenuFilter(v, ib_filter);
+            }
+        });
+
         return view;
 
     }
@@ -32,5 +47,19 @@ public class HomeFeedFragment extends Fragment {
     public void gotoViewPic(View view){
         Intent intent = new Intent(getContext(), ViewPic.class);
         startActivity(intent);
+    }
+
+    public void showPopUpMenuFilter(View view, ImageButton filter){
+        PopupMenu popupMenu = new PopupMenu(getActivity(), filter);
+        popupMenu.getMenuInflater().inflate(R.menu.category_filter_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(getActivity(),"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        popupMenu.show();
     }
 }
